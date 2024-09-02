93e623fd931570fd6877f17259c6f6269b08bfde
kojocrash <57080387+kojocrash@users.noreply.github.com>
2024-09-01 20:13:43 -0700

    settings + performance

--- src/background.js
+++ src/background.js
@@ -5,19 +5,58 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-async function groupTabs() {
-  // Get all Gelbooru tabs
-  let tabs = await chrome.tabs.query({
-    url: "*://*.gelbooru.com/*",
+/* 
+config = {
+  groupNameSuffix: string,
+  targetTabOption: ["all", "currentWindow", "specificWindow", "highlighted"],
+  targetTabWindowOption: string or number,
+  outputWindowOption: string or number
+  groupSelf: boolean,
+  groupWindowMovementBehavior: boolean,
+  singleTabGrouping: boolean,
+  globalArtistTagBlacklist: boolean,
+  groupUnknownArtists: boolean,
+  artistRequest: boolean
+}
+*/
+
+async function groupTabs(config) {
+  // Gather list of relevant tabs
+  let url_pattern = "*://*.gelbooru.com/*"
+  let tabQueryOptions = {
+    url: url_pattern,
     groupId: chrome.tabGroups.TAB_GROUP_ID_NONE,
-    // windowId: chrome.windows.WINDOW_ID_CURRENT
-  })
+  }
+
+  if (config.targetTabOption == "highlighted") {
+    tabQueryOptions.windowId = chrome.windows.WINDOW_ID_CURRENT;
+    tabQueryOptions.highlighted = true;
+    config.outputWindowOption = "tabWindow";
+  } else if (config.targetTabOption == "currentWindow") {
+    tabQueryOptions.windowId = chrome.windows.WINDOW_ID_CURRENT;
+  } else if (config.targetTabOption == "specificWindow") {
+    tabQueryOptions.windowId = config.targetTabWindowOption;
+  }
+
+
+  let tabs = await chrome.tabs.query(tabQueryOptions);
+
+  // Gather list of tabs used for artist tag blacklist
+  var blacklistTabs = undefined;
+
+  if (config.globalArtistTagBlacklist) {
+    blacklistTabs = await chrome.tabs.query({
+      url: url_pattern
+    })
+  } else {
+    blacklistTabs = tabs;
+  }
 
   // Create blacklist
   let tagBlacklist = {};
   let i = 0;
 
-  for (let tab of tabs) {
+  for (let tab of blacklistTabs) {
     // Send current progress
     chrome.runtime.sendMessage({
       type: "STATUS_UPDATE",
@@ -37,22 +76,21 @@ async function groupTabs() {
     }
   }
 
-  // Iterate all Gelbooru tabs
+  // Process all Gelbooru tabs
   let groupingInfo = {};
-  i = 0;
+  let completed = 0;
 
-  for (let tab of tabs) {
-    // Send current progress
-    chrome.runtime.sendMessage({
-      type: "STATUS_UPDATE",
-      payload: {
-        header: "Creating groups...",
-        progress: i,
-        total: tabs.length
-      }
-    });
-    i++;
+  // Send initial notification
+  chrome.runtime.sendMessage({
+    type: "STATUS_UPDATE",
+    payload: {
+      header: "Identifying groups...",
+      progress: 0,
+      total: tabs.length
+    }
+  });
 
+  async function processTab(tab) {
     // Only work on tabs which are image pages or tag search pages
     let isImgTab = tab.title.includes("- Image View -");
     let isSearchTab = tab.title.includes("| Page:");
@@ -61,101 +99,140 @@ async function groupTabs() {
 
     if (isRelevantTab && isUngrouped) {
       let mainTag = tab.title.split("|")[0].split(",")[0].trim();
-      
-      if ((isSearchTab && tagBlacklist[mainTag]) || tab.title.includes("artist request")) {
-        mainTag = "unknown artist"
-      }
-      
-      let groupName = mainTag + " | Gelbooru Group";
-      
-      if (groupingInfo[groupName]) {
-        // Add tab to group info if group info exists
-        groupingInfo[groupName].tabsToAdd.push(tab.id)
-      } else {
-        // Find pre-existing group
-        let foundGroups = await chrome.tabGroups.query({title: groupName});
-  
-        // Create group info for group, create new group if none exist
-        if (foundGroups.length > 0) {
-          groupingInfo[groupName] = {
-            preexisting: true,
-            groupId: foundGroups[0].id,
-            tabsToAdd: [tab.id]
-          }
+      // TODO: remove `isSearchTag` from if statement
+      if ((isSearchTab && tagBlacklist[mainTag]) || (tab.title.includes("artist request") && config.artistRequest)) {
+        if (config.groupUnknownArtists) {
+          mainTag = "unknown artist";
         } else {
-          // Create new group
-          let newGroupId = await chrome.tabs.group({tabIds: tab.id})
-          groupingInfo[groupName] = {
-            preexisting: false,
-            groupId: newGroupId,
-            tabsToAdd: []
-          }
-          
-          // Set group name
-          chrome.tabGroups.update(newGroupId, {
-            title: groupName,
-            color: "blue",
-            collapsed: true
-          });
+          return;
         }
       }
+
+      let groupName = mainTag + config.groupNameSuffix;
+      
+      groupingInfo[groupName] = groupingInfo[groupName] || [];
+      groupingInfo[groupName].push(tab.id);
     }
   }
-  
+
+  let promises = [];
+  for (let tab of tabs) {
+    promises.push(processTab(tab).finally(() => {
+      // Notify current progress
+      completed++;
+      chrome.runtime.sendMessage({
+        type: "STATUS_UPDATE",
+        payload: {
+          header: "Identifying groups...",
+          progress: completed,
+          total: tabs.length
+        }
+      });
+    }));
+  }
+
+  // Wait for all tabs to be processed
+  await Promise.all(promises);
+
   // Group tabs
-  let groupCount = Object.keys(groupingInfo).length;
+  let entries = Object.entries(groupingInfo);
+  let groupCount = entries.length;
+  promises.length = 0;
+  completed = 0;
 
-  if (groupCount > 0) {
-    i = 0;
+  // Send initial notification
+  chrome.runtime.sendMessage({
+    type: "STATUS_UPDATE",
+    payload: {
+      header: "Grouping tabs...",
+      progress: 0,
+      total: groupCount
+    }
+  });
 
-    function updateCount() {
-      if (i < groupCount) {
-        chrome.runtime.sendMessage({
-          type: "STATUS_UPDATE",
-          payload: {
-            header: "Moving remaining tabs into groups...",
-            progress: i,
-            total: groupCount
-          }
-        });
-      } else {
-        chrome.runtime.sendMessage({
-          type: "STATUS_FINISHED",
-          payload: {
-            text: "Finished",
-          }
-        });
-      }
-
-      i++;
+  async function processGroup(groupName, tabsToAdd) {
+    var foundGroups = undefined;
+    if (config.targetTabOption == "highlighted" && config.groupSelf) {
+      foundGroups = [];
+    } else {
+      foundGroups = await chrome.tabGroups.query({title: groupName});
     }
 
-    updateCount();
+    // Create group info for group, create new group if none exist
+    if (foundGroups.length > 0) {
+      let foundTabGroup = foundGroups[0];
+      await chrome.tabs.group({
+        groupId: foundTabGroup.id,
+        tabIds: tabsToAdd
+      });
 
-    for (const groupInfo of Object.values(groupingInfo)) {
-      // Move tabs into group
-      if (groupInfo.tabsToAdd.length > 0) {
-        chrome.tabs.group({
-          groupId: groupInfo.groupId,
-          tabIds: groupInfo.tabsToAdd
-        }, updateCount);
-      } else {
-        updateCount();
+      if (config.groupWindowMovementBehavior && typeof(config.outputWindowOption) == "number" && foundTabGroup.windowId != config.outputWindowOption) {
+        chrome.tabGroups.move(foundTabGroup.id, {
+          index: -1,
+          windowId: config.outputWindowOption
+        });
+      }
+    } else {
+      if (config.singleTabGrouping || tabsToAdd.length > 1) {
+        // Create new group & add tabs to group
+        let groupingQueryOptions = {tabIds: tabsToAdd};
+        if (typeof(config.outputWindowOption) == "number") {
+          groupingQueryOptions.createProperties = {
+            windowId: config.outputWindowOption
+          }
+        }
+
+        let newGroupId = await chrome.tabs.group(groupingQueryOptions);
+        
+        // Set new group's name
+        chrome.tabGroups.update(newGroupId, {
+          title: groupName,
+          color: "blue",
+          collapsed: true
+        });
       }
     }
-  } else {
-    chrome.runtime.sendMessage({
-      type: "STATUS_FINISHED",
-      payload: {
-        text: "Finished",
-      }
-    });
   }
-  
+
+  for (const [groupName, tabsToAdd] of entries) {
+    promises.push(processGroup(groupName, tabsToAdd).finally(() => {
+      completed++;
+      chrome.runtime.sendMessage({
+        type: "STATUS_UPDATE",
+        payload: {
+          header: "Grouping tabs...",
+          progress: completed,
+          total: groupCount
+        }
+      });
+    }));
+  }
+
+  // Wait for all tabs to be processed
+  await Promise.all(promises);
+
+  // Notify of finishing
+  chrome.runtime.sendMessage({
+    type: "STATUS_FINISHED",
+    payload: {
+      text: "Finished",
+    }
+  });
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type == "GROUP_TABS") {
-    groupTabs();
+    groupTabs(request.payload);
   }
 });
+
+function updateDropdowns() {
+  // TODO: update when active tab is changed
+  chrome.runtime.sendMessage({
+    type: "WINDOW_CHANGE",
+    payload: {}
+  });
+}
+
+chrome.windows.onCreated.addListener(updateDropdowns);
+chrome.windows.onRemoved.addListener(updateDropdowns);
--- src/popup.js
+++ src/popup.js
@@ -4,12 +4,27 @@ import "./popup.css";
 
 // Popup code
 
+// Main operation
 function startGroupingSignal() {
   let confirmation = window.confirm("Run Gelbooru Tab Grouper?")
   if (confirmation) {
+    let targetTabWindowOption = isNaN(parseInt(targetWindowTabsSelect.value)) ? targetWindowTabsSelect.value : parseInt(targetWindowTabsSelect.value);
+    let outputWindowOption = isNaN(parseInt(outputWindowSelect.value)) ? outputWindowSelect.value : parseInt(outputWindowSelect.value);
+    
     chrome.runtime.sendMessage({
       type: "GROUP_TABS",
-      payload: {},
+      payload: {
+        groupNameSuffix: groupNameSuffixInput.value.trim(),
+        targetTabOption: targetTabsSelect.value,
+        targetTabWindowOption: targetTabWindowOption,
+        outputWindowOption: outputWindowOption,
+        groupSelf: groupSelfCheckbox.checked,
+        groupWindowMovementBehavior: groupWindowMovementBehaviorCheckbox.checked,
+        singleTabGrouping: singleTabGroupsCheckbox.checked,
+        globalArtistTagBlacklist: globalArtistTagBlacklistCheckbox.checked,
+        groupUnknownArtists: groupUnknownArtistsCheckbox.checked,
+        artistRequest: artistRequestCheckbox.checked
+      },
     });
   }
 }
@@ -33,5 +48,203 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     statusHeader.style.visibility = "visible";
     
     statusBar.style.visibility = "hidden";
+  }
+});
+
+// ========= UI CONSTANTS ========= //
+// Dropdowns
+const targetTabsSelect = document.getElementById("targetTabs");
+const outputWindowSelect = document.getElementById("outputWindow");
+const targetWindowTabsSelect = document.getElementById("targetWindowTabs");
+
+// Text field
+const groupNameSuffixInput = document.getElementById("groupNameSuffix");
+
+// Checkboxes
+const groupSelfCheckbox = document.getElementById("highlightGroupingBehavior");
+const moveGroupsCheckbox = document.getElementById("moveGroupsToWindow");
+const groupWindowMovementBehaviorCheckbox = document.getElementById("groupWindowMovementBehavior");
+const singleTabGroupsCheckbox = document.getElementById("singleTabGroups");
+const globalArtistTagBlacklistCheckbox = document.getElementById("globalArtistTagBlacklist");
+const groupUnknownArtistsCheckbox = document.getElementById("groupUnknownArtists");
+const artistRequestCheckbox = document.getElementById("artistRequest");
+
+// Preview text
+const previewText = document.getElementById("previewText");
+
+// Warning section
+const warningText = document.getElementById("warningText");
+const warningSection = document.getElementById("warnings");
+
+// Setting Items
+const groupSelfSettingItem = document.getElementById("highlightGroupingBehaviorSettingItem");
+const targetWindowSettingItem = document.getElementById("targetWindowTabsSettingItem");
+const moveGroupsSettingItem = document.getElementById("groupWindowMovementBehaviorSettingItem");
+
+// ========= UI FUNCTIONALITY ========= //
+// Show/hide relevant UI
+function updateSubSettingVisibility() {
+    if (targetTabsSelect.value == "highlighted") {
+      groupSelfSettingItem.style.display = "flex";
+      outputWindowSelect.style.display = "none";
+    } else {
+      groupSelfSettingItem.style.display = "none";
+      outputWindowSelect.style.display = "unset";
+    }
+
+    if (targetTabsSelect.value == "specificWindow") {
+      targetWindowSettingItem.style.display = "flex";
+    } else {
+      targetWindowSettingItem.style.display = "none";
+    }
+
+    if (outputWindowSelect.value != "tabWindow") {
+        moveGroupsSettingItem.style.display = "flex";
+    } else {
+        moveGroupsSettingItem.style.display = "none";
+    }
+}
+
+targetTabsSelect.addEventListener("change", updateSubSettingVisibility);
+outputWindowSelect.addEventListener("change", updateSubSettingVisibility);
+updateSubSettingVisibility();
+
+// Update group name preview
+groupNameSuffixInput.addEventListener("input", () => {
+  previewText.innerHTML = "Preview: bonesaw_(rdkshinku)" + groupNameSuffixInput.value;
+});
+
+// Warnings
+function updateWarnings() {
+  let warnings = [];
+
+  // Warning 1
+  if (!groupNameSuffixInput.value.trim()) {
+    warnings.push("● Use a unique group name to significantly reduce the risk of conflicting with your personal tab groups (optional)");
+  }
+
+  // Warning 2
+  let bothWindowDropdownsVisible = targetTabsSelect.value == "specificWindow" && outputWindowSelect.value != "tabWindow";
+  let usingSameWindow = outputWindowSelect.value == "tabWindow" || outputWindowSelect.value == targetWindowTabsSelect.value;
+
+  if (bothWindowDropdownsVisible && !usingSameWindow) {
+    warnings.push("● Targeted window's tabs are different from output window. You sure?");
+  }
+  
+  // Display warning
+  warningSection.style.display = warnings.length ? "unset" : "none"
+  warningText.innerHTML = warnings.join("\n\n");
+
+  console.log(warnings);
+}
+
+outputWindowSelect.addEventListener("change", updateWarnings);
+targetWindowTabsSelect.addEventListener("change", updateWarnings);
+
+// Load settings
+chrome.storage.local.get("settings", function(result) {
+  let settings = result.settings || {};
+	settings.groupNameSuffix = settings.groupNameSuffix != undefined ? settings.groupNameSuffix : "⭐";
+	settings.targetTabOption = settings.targetTabOption != undefined ? settings.targetTabOption : "all";
+	settings.groupSelf = settings.groupSelf != undefined ? settings.groupSelf : false;
+	settings.groupWindowMovementBehavior = settings.groupWindowMovementBehavior != undefined ? settings.groupWindowMovementBehavior : false;
+	settings.singleTabGrouping = settings.singleTabGrouping != undefined ? settings.singleTabGrouping : true;
+	settings.globalArtistTagBlacklist = settings.globalArtistTagBlacklist != undefined ? settings.globalArtistTagBlacklist : true;
+	settings.groupUnknownArtists = settings.groupUnknownArtists != undefined ? settings.groupUnknownArtists : true;
+	settings.artistRequest = settings.artistRequest != undefined ? settings.artistRequest : true;
+
+  groupNameSuffixInput.value = settings.groupNameSuffix;
+  previewText.innerHTML = "Preview: bonesaw_(rdkshinku)" + settings.groupNameSuffix;
+  targetTabsSelect.value = settings.targetTabOption;
+  groupSelfCheckbox.checked = settings.groupSelf;
+  groupWindowMovementBehaviorCheckbox.checked = settings.groupWindowMovementBehavior;
+  singleTabGroupsCheckbox.checked = settings.singleTabGrouping;
+  globalArtistTagBlacklistCheckbox.checked = settings.globalArtistTagBlacklist;
+  groupUnknownArtistsCheckbox.checked = settings.groupUnknownArtists;
+  artistRequestCheckbox.checked = settings.artistRequest;
+
+  updateWarnings();
+  updateSubSettingVisibility();
+});
+
+// Save settings
+function saveSettings() {
+  let settings = {};
+  settings.groupNameSuffix = groupNameSuffixInput.value
+  settings.targetTabOption = targetTabsSelect.value
+  settings.groupSelf = groupSelfCheckbox.checked
+  settings.groupWindowMovementBehavior = groupWindowMovementBehaviorCheckbox.checked
+  settings.singleTabGrouping = singleTabGroupsCheckbox.checked
+  settings.globalArtistTagBlacklist = globalArtistTagBlacklistCheckbox.checked
+  settings.groupUnknownArtists = groupUnknownArtistsCheckbox.checked
+  settings.artistRequest = artistRequestCheckbox.checked
+
+  chrome.storage.local.set({"settings": settings});
+  updateWarnings();
+}
+
+groupNameSuffixInput.addEventListener("input", saveSettings);
+targetTabsSelect.addEventListener("change", saveSettings);
+groupSelfCheckbox.addEventListener("change", saveSettings);
+groupWindowMovementBehaviorCheckbox.addEventListener("change", saveSettings);
+singleTabGroupsCheckbox.addEventListener("change", saveSettings);
+globalArtistTagBlacklistCheckbox.addEventListener("change", saveSettings);
+groupUnknownArtistsCheckbox.addEventListener("change", saveSettings);
+artistRequestCheckbox.addEventListener("change", saveSettings);
+
+// Populate Window Dropdowns
+const outputWindowOptions = [];
+const targetWindowTabsOptions = [];
+
+function populateWindowDropdowns() {
+  const selectedOutputWindowId = outputWindowSelect.value;
+  const selectedTargetWindowTabsId = targetWindowTabsSelect.value;
+  
+  chrome.windows.getAll({
+    populate: true,
+    windowTypes: ["normal"]
+  }, function(windows) {
+    outputWindowOptions.forEach(option => outputWindowSelect.removeChild(option));
+    outputWindowOptions.length = 0;
+
+    targetWindowTabsOptions.forEach(option => targetWindowTabsSelect.removeChild(option));
+    targetWindowTabsOptions.length = 0;
+
+    windows.forEach(window => {
+      if (window.type === "normal") {
+        const activeTab = window.tabs.find(tab => tab.active);
+        const title = activeTab ? activeTab.title : `Window ${window.id}`;
+
+        const option1 = document.createElement("option");
+        option1.value = window.id;
+        option1.textContent = title;
+        
+        outputWindowSelect.appendChild(option1);
+        outputWindowOptions.push(option1);
+
+        if (selectedOutputWindowId == window.id) {
+          outputWindowSelect.value = selectedOutputWindowId;
+        }
+        
+        const option2 = document.createElement("option");
+        option2.value = window.id;
+        option2.textContent = title;
+        
+        targetWindowTabsSelect.appendChild(option2);
+        targetWindowTabsOptions.push(option2);
+
+        if (selectedTargetWindowTabsId == window.id) {
+          targetWindowTabsSelect.value = selectedTargetWindowTabsId;
+        }
+      }
+    });
+  });
+}
+
+populateWindowDropdowns();
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type == "WINDOW_CHANGE") {
+    populateWindowDropdowns();
   }
 });
